aa85d25848c956d305ca72d16c0ef15beff728f0
ThearyCoding <chorntheary12@gamil.com>
2025-03-02 22:32:27 +0700

    update core

--- index.js
+++ index.js
@@ -10,10 +10,16 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, x-api-key");
+  
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
   next();
 });
+
 app.use("/api/products", authMiddleware, require("./routers/productRoutes"));
 app.use("/api/categories", authMiddleware, require("./routers/categoryRoutes"));
 app.use("/api/users", require("./routers/userRoutes"));
